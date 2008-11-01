3addbce3be9800210b83c0b07c20bab78ce2d08f
meyer.hugo <meyer.hugo@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-01 15:42:11 +0000

    Ak subo lo primero a subversion

---
+++ trunk/analizador-lexico/src/analizadorlexico/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 1 de noviembre de 2008, 12:41
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package analizadorlexico;
+
+/**
+ *
+ * @author Huguis
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
