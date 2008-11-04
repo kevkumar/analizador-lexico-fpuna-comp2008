1e47e7fcf3c3112aea846d6d5d12b386f9551e31
lp.poletti <lp.poletti@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-04 23:51:57 +0000

    - Cree el package afd - La clase AFDEquivalente - La clase Estados para manejar los estados
    (Las dos clases estoy implementando teniendo en cuenta nuestros ejemplos, los subi aora nada mas para organizar y no pisarnos)

---
+++ trunk/analizador-lexico/src/analizadorlexico/afd/AFDEquivalente.java
@@ -0,0 +1,25 @@
+/*
+ * AFDEquivalente.java
+ *
+ * Created on 4 de noviembre de 2008, 19:56
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package analizadorlexico.afd;
+
+/**
+ * Clase que representa el AFD Equivalente que se obtiene a partir del AFN Equivalente
+ * @author Polo
+ */   
+public class AFDEquivalente {
+    
+    /*Atributos de la clase*/
+    /**
+     * AFN a partir del cual se generará el AFD equivalente
+     */        
+    public AFDEquivalente() {
+    }
+    
+}
