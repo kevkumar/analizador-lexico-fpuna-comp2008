3f5efa552e4bf41f0635d353f3578e6d5a1ccd0c
lp.poletti <lp.poletti@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-04 23:51:57 +0000

    - Cree el package afd - La clase AFDEquivalente - La clase Estados para manejar los estados
    (Las dos clases estoy implementando teniendo en cuenta nuestros ejemplos, los subi aora nada mas para organizar y no pisarnos)

---
+++ analizador-lexico/src/analizadorlexico/afd/Estados.java
@@ -0,0 +1,22 @@
+/*
+ * Estados.java
+ *
+ * Created on 4 de noviembre de 2008, 20:21
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package analizadorlexico.afd;
+
+/**
+ *
+ * @author Polo
+ */
+public class Estados {
+    
+    /** Creates a new instance of Estados */
+    public Estados() {
+    }
+    
+}
