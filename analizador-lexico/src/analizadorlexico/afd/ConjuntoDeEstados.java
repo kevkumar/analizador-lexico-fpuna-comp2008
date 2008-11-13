c1c3b3cf60c787436f7b9c5da97604c8e4a8948a
lp.poletti <lp.poletti@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-13 23:23:58 +0000

    Actualizacion de clases y creacion de HashNodeTransicion.java

--- analizador-lexico/src/analizadorlexico/afd/ConjuntoDeEstados.java
+++ analizador-lexico/src/analizadorlexico/afd/ConjuntoDeEstados.java
@@ -6,15 +6,80 @@
 package analizadorlexico.afd;
 
 import analizadorlexico.Estado;
+import java.util.List;
 
 /**
  *
  * @author Polo
  */
-class ConjuntoDeEstados {
+public class ConjuntoDeEstados {
+    
+    private List<Estado> lista;
+    
+    private int inicio;
 
-    void addEstado(Estado inicio) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
+    }
+
+    public ConjuntoDeEstados() {
+    }
+
+    /**
+     * @return List<Estado> - Lista de estados
+     */
+    public List<Estado> getLista() {
+        return lista;
+    }
+
+    /**
+     * @param lista - Lista de estados
+     */
+    public void setLista(List<Estado> lista) {
+        this.lista = lista;
+    }
+
+    public ConjuntoDeEstados(List<Estado> lista) {
+        this.lista = lista;
+    }
+        
+    /**
+     * @param inicio - Estado de inicio
+     */
+    public void addEstado(Estado inicio) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+        this.lista.add(inicio);
+    }
+
+    /**
+     * @param nuevo - Contiene estado?     
+     * @return boolean
+     */
+    public boolean contieneEstado(Estado nuevo) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+        /* MENSAJE: Ver comportamiento de contains*/        
+        return this.lista.contains(nuevo);
+    }    
+    
+    public boolean equals(ConjuntoDeEstados comp){
+        if(this.getLista().size() != comp.getLista().size())
+            return false;
+        boolean respuesta = false;
+        for(Estado esteConjunto : this.getLista()){
+            for(Estado comparar : comp.getLista()){
+                if(esteConjunto.equals(comp)){
+                    respuesta = true;
+                    break;
+                }
+            if(!respuesta)
+                return respuesta;    
+            }
+        }
+        return true;
     }
 
 }
