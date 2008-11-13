54a3bdb7f8c74636ce522901a2886b91c92237fd
meyer.hugo <meyer.hugo@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-13 01:42:09 +0000

    Falta solamente implementar la operacion and y or de thompson.

--- trunk/analizador-lexico/src/analizadorlexico/Afn.java
+++ trunk/analizador-lexico/src/analizadorlexico/Afn.java
@@ -9,6 +9,8 @@
 
 package analizadorlexico;
 
+import java.util.Iterator;
+
 /**
  * 
  * @author Huguis
@@ -92,9 +94,101 @@ public class Afn {
             this.setEstadoFinal(eFinalNuevo);
             this.estados.getEstados().add(this.getEstadoInicial());
             this.estados.getEstados().add(this.getEstadoFinal());
+            this.getEstadoInicial().setEInicial(true);
+            this.getEstadoFinal().setEFinal(true);
+            
+        }else if(tipo ==2){
+            /*aumentamos todos los estados en un valor para poder insertar
+             *un nuevo estado inicial*/
+            this.ajustarId(1);
+            
+            /*creamos un estado inicial nuevo y uno final nuevo*/
+            Estado eInicialNuevo = new Estado(0);
+            Estado eFinalNuevo = new Estado(this.estados.getEstados().size()+1);
+            this.estadoInicial.setEInicial(false);
+            this.estadoFinal.setEFinal(false);
+            
+            /*arco con vacio*/
+            Arco arco = new Arco("E",eInicialNuevo,this.estadoInicial);
+            /*añadimos el enlace entre el estado inicial nuevo y el que paso a ser
+             *segundo*/
+            eInicialNuevo.getArcos().add(arco);
+            
+            /*arco con vacio*/
+            arco = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
+            /*añadimos el enlace entre el estado final viejo, y el estado inicial
+             *viejo, esto es para que se de la oportunidad de 1 o más veces.*/
+            this.getEstadoFinal().getArcos().add(arco);
+            
+            /*arco con vacio*/
+            arco = new Arco("E",this.estadoFinal,eFinalNuevo);
+            /*añadimos el enlace entre el estado final viejo, y el estado final
+             *nuevo*/
+            this.getEstadoFinal().getArcos().add(arco);
+            
+            this.setEstadoInicial(eInicialNuevo);
+            this.setEstadoFinal(eFinalNuevo);
+            this.estados.getEstados().add(this.getEstadoInicial());
+            this.estados.getEstados().add(this.getEstadoFinal());
+            
+            this.getEstadoInicial().setEInicial(true);
+            this.getEstadoFinal().setEFinal(true);
+            
+        }else if(tipo == 3){
+            /*aumentamos todos los estados en un valor para poder insertar
+             *un nuevo estado inicial*/
+            this.ajustarId(1);
+            
+            /*creamos un estado inicial nuevo y uno final nuevo*/
+            Estado eInicialNuevo = new Estado(0);
+            Estado eFinalNuevo = new Estado(this.estados.getEstados().size()+1);
+            this.estadoInicial.setEInicial(false);
+            this.estadoFinal.setEFinal(false);
+            
+            /*arco con vacio*/
+            Arco arco = new Arco("E",eInicialNuevo,this.estadoInicial);
+            /*añadimos el enlace entre el estado inicial nuevo y el que paso a ser
+             *segundo*/
+            eInicialNuevo.getArcos().add(arco);
+            
+            /*arco con vacio*/
+            arco = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
+            /*añadimos el enlace entre el estado final viejo, y el estado inicial
+             *viejo, esto es para que se de la oportunidad de 1 o más veces.*/
+            this.getEstadoFinal().getArcos().add(arco);
+            
+            /*arco con vacio*/
+            arco = new Arco("E",this.estadoFinal,eFinalNuevo);
+            /*añadimos el enlace entre el estado final viejo, y el estado final
+             *nuevo*/
+            this.getEstadoFinal().getArcos().add(arco);
+            
+            this.setEstadoInicial(eInicialNuevo);
+            this.setEstadoFinal(eFinalNuevo);
+            this.estados.getEstados().add(this.getEstadoInicial());
+            this.estados.getEstados().add(this.getEstadoFinal());   
+            
+            this.getEstadoInicial().setEInicial(true);
+            this.getEstadoFinal().setEFinal(true);
+            
+            /*ahora añadimos un enlace vacio entre el inicio nuevo y el fin nuevo
+             *para asegurar de que podría darse la no aparición*/
+            arco = new Arco("E",this.getEstadoInicial(),this.getEstadoFinal());
+            this.getEstadoInicial().getArcos().add(arco);
             
         }
     }
+    /**
+     *Lleva a cabo las operaciones binarias de thompson
+     * 1-REALIZA LA OPERACIÓN DE THOMPSON "|" QUE SERÍA UN OR
+     * 2-REALIZA LA OPERACIÓN DE THOMPSON "ab" QUE SERIA UN a AND b
+     * @param afn AFN sobre el cual se trabaja
+     * @param tipo Tipo de operador que se debe realizar, 1 indica |, 2 indica and
+     */
+    public void thompsonOpsBinarias(Afn afn, int tipo){
+        
+    }
+    
     /**
      * ESTA FUNCIÓN NOS PERMITE AJUSTAR LA NUMERACIÓN DE LOS ESTADOS PARA CUANDO
      * INSERTAMOS ESTADOS Y HACE FALTA RE-ENUMERAR LOS ID DE LOS MISMOS
@@ -102,12 +196,11 @@ public class Afn {
      * cantidad de estados que se agregan antes.
      */
      public void ajustarId(int valor){
-        Iterator it = this.estados.getIterator();
+        Iterator it = this.getEstados().getEstados().iterator();
         while (it.hasNext()){
-            Estado e = (Estado) it.next();
-            e.setId(e.getId()+incremento);
+            Estado estadoAux = (Estado) it.next();
+            estadoAux.setIdEstado(estadoAux.getIdEstado()+ valor);
         }
-
     }
     
     
