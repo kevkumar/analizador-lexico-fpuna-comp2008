c5cd7f07d7cdf4c092b07ee99ceefc0b1a4b1a60
meyer.hugo <meyer.hugo@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-14 00:36:54 +0000

    Version 1 del Afn.java, probablemente funciona, falta probar con un main. Ahora falta en la parte del analizador llamar correctamente a las funciones de thompson de acuerdo al BNF.

--- analizador-lexico/src/analizadorlexico/Afn.java
+++ analizador-lexico/src/analizadorlexico/Afn.java
@@ -73,22 +73,22 @@ public class Afn {
             this.estadoFinal.setEFinal(false);
             
             /*arco con vacio*/
-            Arco arco = new Arco("E",eInicialNuevo,this.estadoInicial);
+            Arco arco1 = new Arco("E",eInicialNuevo,this.estadoInicial);
             /*añadimos el enlace entre el estado inicial nuevo y el que paso a ser
              *segundo*/
-            eInicialNuevo.getArcos().add(arco);
+            eInicialNuevo.getArcos().add(arco1);
             
             /*arco con vacio*/
-            arco = new Arco("E",eInicialNuevo,eFinalNuevo);
+            Arco arco2 = new Arco("E",eInicialNuevo,eFinalNuevo);
             /*añadimos el enlace entre el estado inicial nuevo, y el estado final
              *nuevo*/
-            eInicialNuevo.getArcos().add(arco);
+            eInicialNuevo.getArcos().add(arco2);
             
             /*arco con vacio*/
-            arco = new Arco("E",this.estadoFinal,eFinalNuevo);
+            Arco arco3 = new Arco("E",this.estadoFinal,eFinalNuevo);
             /*añadimos el enlace entre el estado final viejo, y el estado final
              *nuevo*/
-            this.getEstadoFinal().getArcos().add(arco);
+            this.getEstadoFinal().getArcos().add(arco3);
             
             this.setEstadoInicial(eInicialNuevo);
             this.setEstadoFinal(eFinalNuevo);
@@ -109,22 +109,22 @@ public class Afn {
             this.estadoFinal.setEFinal(false);
             
             /*arco con vacio*/
-            Arco arco = new Arco("E",eInicialNuevo,this.estadoInicial);
+            Arco arco1 = new Arco("E",eInicialNuevo,this.estadoInicial);
             /*añadimos el enlace entre el estado inicial nuevo y el que paso a ser
              *segundo*/
-            eInicialNuevo.getArcos().add(arco);
+            eInicialNuevo.getArcos().add(arco1);
             
             /*arco con vacio*/
-            arco = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
+            Arco arco2 = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
             /*añadimos el enlace entre el estado final viejo, y el estado inicial
              *viejo, esto es para que se de la oportunidad de 1 o más veces.*/
-            this.getEstadoFinal().getArcos().add(arco);
+            this.getEstadoFinal().getArcos().add(arco2);
             
             /*arco con vacio*/
-            arco = new Arco("E",this.estadoFinal,eFinalNuevo);
+            Arco arco3 = new Arco("E",this.estadoFinal,eFinalNuevo);
             /*añadimos el enlace entre el estado final viejo, y el estado final
              *nuevo*/
-            this.getEstadoFinal().getArcos().add(arco);
+            this.getEstadoFinal().getArcos().add(arco3);
             
             this.setEstadoInicial(eInicialNuevo);
             this.setEstadoFinal(eFinalNuevo);
@@ -146,22 +146,22 @@ public class Afn {
             this.estadoFinal.setEFinal(false);
             
             /*arco con vacio*/
-            Arco arco = new Arco("E",eInicialNuevo,this.estadoInicial);
+            Arco arco1 = new Arco("E",eInicialNuevo,this.estadoInicial);
             /*añadimos el enlace entre el estado inicial nuevo y el que paso a ser
              *segundo*/
-            eInicialNuevo.getArcos().add(arco);
+            eInicialNuevo.getArcos().add(arco1);
             
             /*arco con vacio*/
-            arco = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
+            Arco arco2 = new Arco("E",this.getEstadoFinal(),this.getEstadoInicial());
             /*añadimos el enlace entre el estado final viejo, y el estado inicial
              *viejo, esto es para que se de la oportunidad de 1 o más veces.*/
-            this.getEstadoFinal().getArcos().add(arco);
+            this.getEstadoFinal().getArcos().add(arco2);
             
             /*arco con vacio*/
-            arco = new Arco("E",this.estadoFinal,eFinalNuevo);
+            Arco arco3 = new Arco("E",this.estadoFinal,eFinalNuevo);
             /*añadimos el enlace entre el estado final viejo, y el estado final
              *nuevo*/
-            this.getEstadoFinal().getArcos().add(arco);
+            this.getEstadoFinal().getArcos().add(arco3);
             
             this.setEstadoInicial(eInicialNuevo);
             this.setEstadoFinal(eFinalNuevo);
@@ -173,8 +173,8 @@ public class Afn {
             
             /*ahora añadimos un enlace vacio entre el inicio nuevo y el fin nuevo
              *para asegurar de que podría darse la no aparición*/
-            arco = new Arco("E",this.getEstadoInicial(),this.getEstadoFinal());
-            this.getEstadoInicial().getArcos().add(arco);
+            Arco arco4 = new Arco("E",this.getEstadoInicial(),this.getEstadoFinal());
+            this.getEstadoInicial().getArcos().add(arco4);
             
         }
     }
@@ -191,7 +191,100 @@ public class Afn {
          *trabajamos*/
         if(tipo == 1){
             Estado eInicialNuevo = new Estado(0);
-            Estado eFinalNuevo = new Estado(this.estados.getEstados().size()+afnAUnir.estados.getEstados().size()+1);
+            Estado eFinalNuevo = new Estado(this.estados.getEstados().size()+ 
+                afnAUnir.estados.getEstados().size()+1);
+            
+            /*Ahora lo que se hace es crear los clásicos enlaces vacios que
+             *propone thompson entre el nuevo inicio y los dos inicios de cada
+             *uno de los automatas(this y afnAUnir*/
+            Arco arco1 = new Arco("E",eInicialNuevo,this.getEstadoInicial());
+            eInicialNuevo.getArcos().add(arco1);
+            
+            Arco arco2 = new Arco("E",eInicialNuevo,afnAUnir.getEstadoInicial());
+            eInicialNuevo.getArcos().add(arco2);
+            
+            /*Ahora lo que se hace es crear los clásicos enlaces vacios que
+             *propone thompson entre el fin de cada uno de los automatas(this y
+             *afnAUnir) y el nuevo estado final.*/
+            Arco arco3 = new Arco("E",this.estadoFinal,eFinalNuevo);
+            this.estadoFinal.getArcos().add(arco3);
+            
+            Arco arco4 = new Arco("E",afnAUnir.estadoFinal,eFinalNuevo);
+            afnAUnir.estadoFinal.getArcos().add(arco4);
+            
+            /* Ahora restablecemos quienes son estados finales e iniciales
+             * respectivamente, los estados finales e iniciales de los 2 automatas
+             * en cuestion ya no los son, ahora los nuevos iniciales son los creados
+             * más arriba*/
+            this.estadoInicial.setEInicial(false);
+            this.estadoFinal.setEFinal(false);
+            
+            afnAUnir.estadoInicial.setEInicial(false);
+            afnAUnir.estadoFinal.setEFinal(false);
+            
+            eInicialNuevo.setEInicial(true);
+            eFinalNuevo.setEFinal(true);
+            
+            /*Nos queda unir el afn this al afn recibido como parametro, para 
+             *hacer esto, primero ajustamos los id's de los estados, para que luego
+             *de la unión estos queden correctamente numerados*/
+            this.ajustarId(1);//para compensar el estado inicial
+            //para compensar el añadido del primer automata al inicio de este.
+            afnAUnir.ajustarId(this.estados.getEstados().size()+1);
+            
+            /* Ahora procedemos a unir ambos automatas para lo cual tomamos cada
+             uno de los estados del afnAUnir y los ponemos dentro de this*/
+            Iterator it = afnAUnir.estados.getEstados().iterator();
+            while(it.hasNext()){
+                this.estados.getEstados().add((Estado) it.next());
+            }
+            /*Insertamos los estados dentro del automata resultante*/
+            this.estados.getEstados().add(eInicialNuevo);
+            this.estados.getEstados().add(eFinalNuevo);
+            
+         /*Si llega de tipo 2 hacemos un AND entre automatas*/   
+        }else if(tipo == 2){
+            /*Lo primero que hacemos es ajustar los id's de los estados del afnAUnir
+             *para poder juntarlo con this.
+             *Lo que hacemos es restar 1 porque el último estado de this no se utiliza
+             * más*/
+            afnAUnir.ajustarId(this.estados.getEstados().size() - 1);
+            
+            /*Tenemos 2 opciones, una es ignorar el último estado de this o ignorar
+             el primer estado de afnAUnir al construir this, la opción que
+             usaremos es la primera, para la cual debemos poner todos los enlaces que
+             tenía el inicial del 2º automata con el fin del primer automata.
+             Es decir, convertimos al nodo final del primer automata en el inicial
+             del 2º. Entonces, cambiamos los enlaces con un iterador*/
+            
+            
+            Iterator<Arco> it = afnAUnir.estadoInicial.getArcos().iterator();
+            while(it.hasNext()){
+                Arco aux = it.next();
+                aux.setOrigen(this.estadoFinal);
+                this.estadoFinal.getArcos().add(aux);
+            }
+            /*Ahora iteramos sobre afnAUnir y metemos los estados en this, con 
+             *excepción del primero, cuyos enlaces ya fueron cambiados.*/
+            Iterator<Estado> itEstados = afnAUnir.estados.getEstados().iterator();
+            while(itEstados.hasNext()){
+                /*Si algún estado apuntaba al estado inicial de afnAUnir, lo cambiamos
+                 *para que apunte al final de this..*/
+                Estado estadoAux = itEstados.next();
+                Iterator<Arco> itArco = itEstados.next().getArcos().iterator();
+                if(!estadoAux.isEInicial()){
+                    while(itArco.hasNext()){
+                        Arco arcoAux= itArco.next();
+                        if(arcoAux.getDestino().getIdEstado() == afnAUnir.estadoInicial.getIdEstado()){
+                            arcoAux.setDestino(this.estadoFinal);
+                        }
+                    }
+                    this.getEstados().getEstados().add(estadoAux);
+                }
+            }
+            this.estadoFinal.setEFinal(false);
+            this.setEstadoFinal(afnAUnir.getEstadoFinal());
+            this.getEstadoFinal().setEFinal(true);
         }
         
     }
@@ -199,8 +292,8 @@ public class Afn {
     /**
      * ESTA FUNCIÓN NOS PERMITE AJUSTAR LA NUMERACIÓN DE LOS ESTADOS PARA CUANDO
      * INSERTAMOS ESTADOS Y HACE FALTA RE-ENUMERAR LOS ID DE LOS MISMOS
-     * @param valor valor o cantidad que se debe sumar a los id de cada estados, de acuerdo a la
-     * cantidad de estados que se agregan antes.
+     * @param valor valor o cantidad que se debe sumar a los id de cada estados, 
+     * de acuerdo a la cantidad de estados que se agregan antes.
      */
      public void ajustarId(int valor){
         Iterator it = this.getEstados().getEstados().iterator();
