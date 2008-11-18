270d14013bae9a05a99159b5028603b9ca193cfe
meyer.hugo <meyer.hugo@a76ee82e-a823-11dd-9c66-c9f8973bfffa>
2008-11-18 03:31:35 +0000

    leer AfdMin.java en la linea 186, falta la funcion que actualice los grupos a los q se apunta de acuerdo a la matriz...

--- analizador-lexico/src/analizadorlexico/afd/AfdMin.java
+++ analizador-lexico/src/analizadorlexico/afd/AfdMin.java
@@ -55,11 +55,14 @@ public class AfdMin {
         Iterator itConjEstados = this.afd.getEstadosMarcados().iterator();
         
         while(itConjEstados.hasNext()){
+            
             ConjuntoDeEstados aux = (ConjuntoDeEstados) itConjEstados.next();
             if(!aux.isConjFinal()){
+                aux.setIdGrupo(1);
                 noFinales.getGrupo().add(aux);
                 this.vectorGrupo[aux.getInicio()] = 1;
             }else{
+                aux.setIdGrupo(0);
                 this.vectorGrupo[aux.getInicio()] = 0;
             }
         }
@@ -68,64 +71,120 @@ public class AfdMin {
         boolean continuar =true;
         /*guarda el valor del caracter leido de la matriz*/
         String valorCaracter;
-        
+        /*SIMPLE CONTADOR PARA VERIFICAR LA CANTIDAD DE ACIERTOS DE LAS LETRAS*/
+        int cont =0;
+        this.inicializarMatriz();
         while(continuar){
             int tamañoConjInicial = this.conjuntoInicial.size();
             for(Grupo grupo : this.conjuntoInicial){
                 if(grupo.getGrupo().size() > 1){
                     
-                    boolean seCreoNuevoGrupo = false;
+                    boolean seCreoNuevoGrupo = true;
                     contGrupo++;
                     grupoAIntroducir = new Grupo(contGrupo);
                     Iterator<ConjuntoDeEstados> itElemGrupo = grupo.getGrupo().iterator();
                     
                     while(itElemGrupo.hasNext()){
                         ConjuntoDeEstados conjAux = itElemGrupo.next();
-                        /*Tomamos un caracter del alfabeto y lo revisamos*/
-                        for(String caracter : alfa){
-                            /*TOMAMOS UN ELEMENTO DE NUESTRO GRUPO, Y VEMOS SI NO APUNTA
-                             *A ALGUN ELEMENTO QUE ESTA FUERA DE NUESTRO GRUPO, SI ES ASI
-                             *LO DEBEMOS PONER EN OTRO GRUPO NUEVO O YA EXISTENTE QUE TENGA
-                             *ELEMENTOS QUE APUNTE AL MISMO GRUPO*/
-                            for(int j=0; j<grupo.getGrupo().size()+1;j++){
-                                
-                                valorCaracter = this.afd.getAdyacencia()[conjAux.getInicio()][j];
-                                /*Si el valor de la matriz de adyacencia es igual al caracter que 
-                                 *estamos examinando entoncs vemos a que grupo apunta nuestro
-                                 *conj aux con ese caracter*/
-                                if(caracter.equalsIgnoreCase(valorCaracter)){
-                                    
-                                    if(vectorGrupo[conjAux.getInicio()] != vectorGrupo[j]){
-                                        
-                                        
-                                    /*CREAR NUEVO GRUPO Y AÑADIRLO AHI, ACTUALIZAR
-                                     *ADEMAS EL VECTORGRUPO Y REMOVER AL CONJAUX
-                                     *DEL GRUPO ACTUAL EN EL QUE ESTA.*/
-                                        seCreoNuevoGrupo =true;
-                                        grupoAIntroducir.getGrupo().add(conjAux);
-                                        //grupo.getGrupo().remove(conjAux);
-                                        itElemGrupo.remove();
-                                        //vectorGrupo[conjAux.getInicio()] = contGrupo;
-                                    }
+                        /*Si es un elemento ya añadido al grupo actual, lo remuevo,
+                         *sino creo un nuevo grupo, dado que los que ya fueron
+                         *cargados en grupo a introducir son todos los que apuntan a un mismo
+                         *grupo*/
+                        if(grupoAIntroducir.getGrupo().contains(conjAux)){
+                            itElemGrupo.remove();
+                            continue;
+                        }else if(grupoAIntroducir.getGrupo().size() > 0){
+                            this.conjuntoActual.add(grupoAIntroducir);
+                            contGrupo++;
+                            grupoAIntroducir = new Grupo(contGrupo);
+                        }
+                        /*TOMADO EL ESTADO LO REMUEVO*/
+                        itElemGrupo.remove();
+                        grupoAIntroducir.getGrupo().add(conjAux);
+                        /*CREO UN NUEVO ITERADOR SOBRE LOS ELEMENTOS RESTANTES EN EL GRUPO*/
+                        Iterator<ConjuntoDeEstados> itNextEGrupo = grupo.getGrupo().iterator();
+                        
+                        /*SI YA NO AGARRAMOS MAS ESTADOS PORQUE YA ES EL ULTIMO, CARGAMOS
+                         *SU GRUPO EN CONJUNTO ACTUAL*/
+                        if(!itNextEGrupo.hasNext()){
+                            this.conjuntoActual.add(grupoAIntroducir);
+                        }
+                        while(itNextEGrupo.hasNext()){
+                            ConjuntoDeEstados conjAux2 = itNextEGrupo.next();
+                            cont = 0;
+                            for(String caracter : alfa){
+                                /*verificamos si apuntan al mismo grupo ambos con la misma variable*/
+                                boolean aComparar =this.matrizEstAlf[conjAux.getInicio()][conseguirIdLetra(caracter)].getIdGrupo() == this.matrizEstAlf[conjAux2.getInicio()][conseguirIdLetra(caracter)].getIdGrupo();
+                                if(aComparar){
+                                    cont++;
                                 }
                             }
+                            /*VERIFICAMOS SI SE CUMPLIO QUE APUNTAN AL MISMOS GRUPOS CON LAS MISMAS LETRAS*/
+                            if(cont == alfa.size()){
+                                grupoAIntroducir.getGrupo().add(conjAux2);
+                                //itNextEGrupo.remove();
+                                
+                            }
                         }
+                        
+                        /*Tomamos un caracter del alfabeto y lo revisamos*/
+//                        for(String caracter : alfa){
+//                            /*TOMAMOS UN ELEMENTO DE NUESTRO GRUPO, Y VEMOS SI NO APUNTA
+//                             *A ALGUN ELEMENTO QUE ESTA FUERA DE NUESTRO GRUPO, SI ES ASI
+//                             *LO DEBEMOS PONER EN OTRO GRUPO NUEVO O YA EXISTENTE QUE TENGA
+//                             *ELEMENTOS QUE APUNTE AL MISMO GRUPO*/
+//                            
+//                            
+//                            for(int j=0; j<grupo.getGrupo().size()+1;j++){
+//                                
+//                                valorCaracter = this.afd.getAdyacencia()[conjAux.getInicio()][j];
+//                                /*Si el valor de la matriz de adyacencia es igual al caracter que 
+//                                 *estamos examinando entoncs vemos a que grupo apunta nuestro
+//                                 *conj aux con ese caracter*/
+//                                if(caracter.equalsIgnoreCase(valorCaracter)){
+//                                    
+//                                    if(vectorGrupo[conjAux.getInicio()] != vectorGrupo[j]){
+//                                        
+//                                        
+//                                    /*CREAR NUEVO GRUPO Y AÑADIRLO AHI, ACTUALIZAR
+//                                     *ADEMAS EL VECTORGRUPO Y REMOVER AL CONJAUX
+//                                     *DEL GRUPO ACTUAL EN EL QUE ESTA.*/
+//                                        seCreoNuevoGrupo =true;
+//                                        grupoAIntroducir.getGrupo().add(conjAux);
+//                                        //grupo.getGrupo().remove(conjAux);
+//                                        itElemGrupo.remove();
+//                                        //vectorGrupo[conjAux.getInicio()] = contGrupo;
+//                                    }
+//                                }
+//                            }
+//                        }
                     }
                     /*Si no se creo un nuevo grupo lo que hacemos es regresar
                      *el valor del contador de grupo, si se creo un nuevo
                      *grupo lo añadimos a conjunto inicial.*/
-                    if(!seCreoNuevoGrupo){
-                        contGrupo--;
-                    }else{
-                        this.conjuntoInicial.add(grupoAIntroducir);
-                        break;
-                    }
+                    //this.conjuntoActual.add(grupoAIntroducir);
+//                    if(!seCreoNuevoGrupo){
+//                        contGrupo--;
+//                    }else{
+//                        this.conjuntoInicial.add(grupoAIntroducir);
+//                        seCreoNuevoGrupo = false;
+//                    }
+                }else{
+                    this.conjuntoActual.add(grupo);
                 }
+                
             }
-            this.actualizarVector();
+           // this.actualizarVector();
             /*Si no se crearon nuevos grupos, terminamos el algoritmo*/
-            if(tamañoConjInicial == this.conjuntoInicial.size())
+            if(tamañoConjInicial == this.conjuntoActual.size()){
                 continuar=false;
+            }else{
+                conjuntoInicial = conjuntoActual;
+                conjuntoActual = new ArrayList<Grupo>();
+            }
+            
+            /*AK FALTA UNA FUNCIÓN ACTUALIZAR GRUPOS, QUE ACTUALICE LOS VALORES
+             *DE LA MATRIZ*/
         }
         
         return this;
@@ -140,11 +199,16 @@ public class AfdMin {
             for(int j=0; j<tamanoFila;j++){
                 letra =this.afd.getAdyacencia()[i][j];
                 if (letra != null){
-                    this.matrizEstAlf[i][this.conseguirIdLetra(letra)] = this.getAfd().getEstadosMarcados().get(i);
+                   // this.conjuntoInicial
+                    if(this.getAfd().getEstadosMarcados().get(j).isConjFinal()){
+                        this.getAfd().getEstadosMarcados().get(j).setIdGrupo(0);
+                    }else{
+                        this.getAfd().getEstadosMarcados().get(j).setIdGrupo(1);
+                    }
+                    this.matrizEstAlf[i][this.conseguirIdLetra(letra)] = this.getAfd().getEstadosMarcados().get(j);
                 }
             }
         }
-        
     }
     public int conseguirIdLetra(String letra){
         int pos =-1;
